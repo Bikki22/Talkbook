7dbda6689315f13920a2474a0e04f1d5ef8de027
Bikki chaudhary <cbikki35@gmail.com>
2026-08-06 18:06:25 +0545

    project init

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
